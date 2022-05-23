b4c42507f46af6c4d6450081f33cc862f95ddaf9
Kittipod Lambangchang <tonkittipod@gmail.com>
2022-05-24 02:23:34 +0700

    Add New Project

---
+++ Time tracking dashboard/src/App.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import { store } from "./lib/Store/store";
+  import Card from "./lib/Components/card.svelte";
+  import "./App.css"
+</script>
+
+<div class="container">
+  <div class="grid-container">
+    {#each $store as item}
+      <div>
+        <h3>{item.title}</h3>
+        <h1>{item.timeframes.weekly.current}</h1>
+        <p>Last Week - {item.timeframes.weekly.previous}</p>
+      </div>
+    {/each}
+  </div>
+</div>
