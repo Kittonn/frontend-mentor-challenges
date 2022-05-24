f5b3ff4c6c716dd175c59774ce990cbe6c185f8c
Kittipod Lambangchang <tonkittipod@gmail.com>
2022-05-24 17:02:08 +0700

    Update Css

--- Time tracking dashboard/src/App.svelte
+++ Time tracking dashboard/src/App.svelte
@@ -2,15 +2,37 @@
   import { store } from "./lib/Store/store";
   import Card from "./lib/Components/card.svelte";
   import "./App.css"
+  let select = ["Daily","Weekly","Monthly"]
+  // let split_data = $store.filter(item=>item.)
 </script>
 
 <div class="container">
   <div class="grid-container">
-    {#each $store as item}
-      <div>
-        <h3>{item.title}</h3>
-        <h1>{item.timeframes.weekly.current}</h1>
-        <p>Last Week - {item.timeframes.weekly.previous}</p>
+    <div class="item-1" id="item-1">
+      <div class="item-1-top">
+        <img src="./images/image-jeremy.png" class="profile-img" alt="">
+        <p>Report for</p>
+        <div class="name">
+          <h1>Jeremy</h1>
+        <h1>Robson</h1>
+        </div>
+      </div>
+      <div class="item-1-bottom">
+        {#each select as item}
+          <button>{item}</button>
+        {/each}
+      </div>
+    </div>
+    {#each $store as item }
+      <div class={item.title}>
+        <div class="content">
+          <div class="content-top">
+            <p>{item.title}</p>
+            <img src="./images/icon-ellipsis.svg" alt="">
+          </div>
+        <p class="content-center">{item.timeframes.weekly.current}hrs</p>
+        <p class="content-footer">Last Week - {item.timeframes.weekly.previous}hrs</p>
+        </div>
       </div>
     {/each}
   </div>
