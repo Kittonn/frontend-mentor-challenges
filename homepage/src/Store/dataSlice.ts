5701c40c3a842001f78c86f75c5c2582593a04f0
Kittipod Lambangchang <tonkittipod@gmail.com>
2022-06-02 20:46:07 +0700

    Update data

--- homepage/src/Store/dataSlice.ts
+++ homepage/src/Store/dataSlice.ts
@@ -1,14 +1,22 @@
 import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
+import {split_data} from '../function'
 let url =
   "https://raw.githubusercontent.com/Kittonn/Frontend-Mentor-Challenges/master/readme_data.md";
 export const getData = createAsyncThunk("/api", async () => {
   return await fetch(url)
     .then((res) => res.text())
-    .then((data) => data.split("\n"));
+    .then((data) => split_data(data));
 });
 
+interface projectI {
+    id:number
+    name:string
+    path:string
+    stack:string[]
+}
+
 interface dataI {
-  data: string[];
+  data: projectI[];
   status: string;
 }
 
---
+++ homepage/src/function.ts
@@ -0,0 +1,19 @@
+export const split_data = (data: string) => {
+  let split_arr = data
+    .split("\n")
+    .filter((item) => item !== "")
+    .slice(4)
+    .map((item) => item.split(" | "));
+
+  let data_arr = split_arr.map((item, i) => {
+    let start_link = item[0].indexOf("(");
+    let end_link = item[0].indexOf(")");
+    let start_name = item[1].indexOf("[");
+    let end_name = item[1].indexOf("]");
+    let path = item[0].substring(start_link + 1, end_link);
+    let name = item[1].substring(start_name + 1, end_name);
+    let stack = item[2].trim().split(" ");
+    return { id: i, name, stack, path };
+  });
+  return data_arr;
+};
