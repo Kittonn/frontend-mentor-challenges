83f1b8a281ce7fd8db565b2b9f723e13c24ba153
Kittipod Lambangchang <tonkittipod@gmail.com>
2022-06-12 14:39:51 +0700

    ⛑ Refactor Qr-Code Component

---
+++ qr-code component/src/App.svelte
@@ -0,0 +1,17 @@
+<!-- 
+ 
+ -->
+<script lang="ts">
+  import Image from "./assets/images/image-qr-code.png";
+</script>
+
+<main class="min-h-screen flex justify-center items-center p-5 bg-[#d6e2f0]">
+  <div class="bg-white text-center rounded-2xl p-4 max-w-[325px]">
+    <img src={Image} class="w-full rounded-xl" alt="" />
+    <h1 class="px-2 mt-5 leading-7 mb-3 text-[22px] font-bold text-[#1f3251]">Improve your front-end skills by building projects</h1>
+    <p class="px-2 mb-5 text-[#7b879d]">
+      Scan the QR code to visit Frontend Mentor and take your coding skills to
+      the next level
+    </p>
+  </div>
+</main>
