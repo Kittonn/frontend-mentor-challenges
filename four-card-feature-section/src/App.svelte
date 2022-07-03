ed9320358a44aae9000e2be1b744b67d9392fc4e
Kittipod Lambangchang <tonkittipod@gmail.com>
2022-07-03 13:42:24 +0700

    🎠 Add : New Challenges

---
+++ four-card-feature-section/src/App.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+  import IconSuper from "./assets/images/icon-supervisor.svg";
+  import IconBuilder from "./assets/images/icon-team-builder.svg";
+  import IconCal from "./assets/images/icon-calculator.svg";
+  import IconKarma from "./assets/images/icon-karma.svg";
+</script>
+
+<main class="bg-[#fafafa] min-h-screen p-5 flex flex-col items-center">
+  <section class="text-center">
+    <h1 class="font-extralight mt-12 mb-2">Reliable, efficient delivery</h1>
+    <h1 class="text-[#4c4e61]">Powered by Technology</h1>
+    <p class="font-normal text-sm md:text-base max-w-[300px] md:max-w-[550px] mt-6">
+      Our Artificial Intelligence powered tools use millions of project data
+      points to ensure that your project is successful
+    </p>
+  </section>
+  <section class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 max-w-[350px] md:max-w-[800px] lg:max-w-[1100px] my-12">
+    <div class="lg:row-span-2 bg-white lg:self-center shadow-lg border-t-[#45d3d3] border-t-4 p-6 rounded-md">
+      <h3>Supervisor</h3>
+      <p>Monitors activity to identify project roadblocks</p>
+      <img src={IconSuper} alt="" />
+    </div>
+    <div class="bg-white border-t-4 border-t-[#ea5353] shadow-lg p-6 rounded-md">
+      <h3>Team Builder</h3>
+      <p>
+        Scans our talent network to create the optimal team for your project
+      </p>
+      <img src={IconBuilder} alt="" />
+    </div>
+    <div class="bg-white lg:row-span-2 lg:self-center border-t-4 shadow-lg border-t-[#549ef2] p-6 rounded-md">
+      <h3>Calculator</h3>
+      <p>Uses data from past projects to provide better delivery estimates</p>
+      <img src={IconCal} alt="" />
+    </div>
+    <div class="bg-white border-t-4 border-t-[#fcaf4a] shadow-lg p-6 rounded-md">
+      <h3>Karma</h3>
+      <p>Regularly evaluates our talent to ensure quality</p>
+      <img src={IconKarma} alt="" />
+    </div>
+  </section>
+</main>
---
+++ product-preview-card-component/src/App.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+  import ImagePC from "./assets/images/image-product-desktop.jpg";
+  import ImageMobile from "./assets/images/image-product-mobile.jpg"
+  import Icon from "./assets/images/icon-cart.svg";
+</script>
+
+<main
+  class="min-h-screen p-5 mx-auto bg-[#f2ebe3] flex justify-center items-center"
+>
+  <div
+    class="grid grid-cols-1 md:grid-cols-2 max-w-[400px] md:max-w-[700px] bg-white rounded-xl overflow-hidden"
+  >
+    <img class="w-full hidden md:block" src={ImagePC} alt="" />
+    <img src={ImageMobile} class='w-full md:hidden' alt="">
+    <div class="p-8">
+      <h5 class="uppercase font-medium text-sm text-[#6c7289] tracking-[0.3em]">perfume</h5>
+      <h1 class="my-4 md:my-8 font-['Fraunces'] text-[#1c232b] font-bold text-3xl md:text-4xl">
+        Gabrielle Essence Eau De Parfum
+      </h1>
+      <p class="font-medium text-sm md:text-base text-[#6c7289]">
+        A floral, solar and voluptuous interpretation composed by Olivier Polge,
+        Perfumer-Creator for the House of CHANEL.
+      </p>
+      <div class="flex items-center mt-10 mb-4 md:my-8 flex-wrap">
+        <h2 class="font-['Fraunces'] mr-8 text-[#3c8067] font-bold text-3xl">
+          $149.99
+        </h2>
+        <h6 class="line-through text-sm text-[#6c7289]">$169.99</h6>
+      </div>
+      <button class="font-bold text-sm rounded-lg text-white w-full py-4 bg-[#3c8067]"
+        ><img src={Icon} class="inline" alt="" /> Add to Cart</button
+      >
+    </div>
+  </div>
+</main>
